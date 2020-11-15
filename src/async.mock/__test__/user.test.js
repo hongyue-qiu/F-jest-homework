f6a9f516566e58ebf401b752f226cba88377ea99
hongyue-qiu <994059641@qq.com>
2020-11-15 11:07:19 +0800

    finish

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,27 @@
+import axios from "axios";
 import { register } from "../user";
+import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    const username = "749";
+    const password = "111";
+    expect(await register(username, password)).toEqual({});
+    expect(axios.post).toHaveBeenCalledWith("/user", {
+      username,
+      password,
+    });
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyUsername.mockImplementation(() => false);
+    const username = "749";
+    const password = "111";
+    await expect(register(username, password)).rejects.toThrow();
   });
 });
